56cbf680aa587d4f779fa6fd7e871e58f35dd010
Joao Melo <joaohfpmelo@gmail.com>
2023-10-23 17:27:03 +0200

    deleted evaluate function from training

--- src/modelling/training.py
+++ src/modelling/training.py
@@ -1,9 +1,6 @@
-from typing import Tuple
-
 import numpy as np
 import pandas as pd
 import xgboost as xgb
-from sklearn.metrics import mean_squared_error, r2_score
 
 
 def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> xgb.XGBRegressor:
@@ -22,9 +19,7 @@ def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> xgb.XGBRegressor:
             Trained XGboost model.
     """
     model = xgb.XGBRegressor()
-
     model.fit(X_train, y_train)
-
     return model
 
 
@@ -45,25 +40,3 @@ def predict(X_data: pd.DataFrame, model: xgb.XGBRegressor) -> np.ndarray:
     """
     preds = model.predict(X_data)
     return preds
-
-
-def evaluate_model(y_true: pd.Series, y_pred: pd.Series) -> Tuple[float, float]:
-    """Evaluate the model by calculating Mean Squared Error (MSE) and R-squared (R2) scores.
-
-    Parameters
-    -----------
-        y_true : pd.Series
-                 The true target values.
-        y_pred : pd.Series
-                 The predicted target values.
-
-    Returns
-    --------
-        mse : float
-              The Mean Squared Error (RMSE) between the true and predicted values.
-        r2 : float
-             The R-squared (R2) score, which measures the goodness of fit of the model.
-    """
-    mse = mean_squared_error(y_true, y_pred, squared=False)
-    r2 = r2_score(y_true, y_pred)
-    return mse, r2
