21c64dcec81fd8c8217c0bc76fe25c0f9d81d717
Joao Melo <joaohfpmelo@gmail.com>
2023-10-23 11:31:16 +0200

    recommiting utils and added changes in requirements

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,42 +1,40 @@
 import pickle
-from typing import Any
+
+from sklearn.linear_model import LinearRegression
+
 
 def load_pickle(path: str) -> pickle:
-    """
-    Given a path, loads the pickle object located in that path.
+    """Given a path, loads the pickle object found in that path.
 
     Parameters
     ----------
     path : string
            String represents the path to the object.
 
-    Returns
+    Returns:
     -------
     loaded_obj: pickle object
                 Pickle which was contained in the path given as parameter.
     """
-
-    with open(path,"rb") as f:
+    with open(path, "rb") as f:
         loaded_obj = pickle.load(f)
     return loaded_obj
 
 
-def save_pickle(path: str, obj: Any) -> None:
-    """
-    Given a path and an object, stores the object as a pickle file in the specified path.
+def save_pickle(path: str, obj: LinearRegression) -> None:
+    """Given a path and an object, stores the object as a pickle file in the specified path.
 
     Parameters
     ----------
     path : string
            String represents the path where the pickle object will be stored.
 
-    obj : any
-          Represents the object that will be stored.
+    obj : LinearRegression
+          Represents the linear regression model that will be stored.
 
-    Returns
+    Returns:
     -------
     None : None
     """
-
     with open(path, "wb") as f:
         pickle.dump(obj, f)
