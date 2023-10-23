3188e72f4c724d4772816e4746cff2b47ca477af
Joao Melo <joaohfpmelo@gmail.com>
2023-10-23 10:58:02 +0200

    Added pickle functions to utils.py

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,1 +1,42 @@
-# Use this module to code a `pickle_object` function. This will be useful to pickle the model (and encoder if need be).
+import pickle
+from typing import Any
+
+def load_pickle(path: str) -> pickle:
+    """
+    Given a path, loads the pickle object located in that path.
+
+    Parameters
+    ----------
+    path : string
+           String represents the path to the object.
+
+    Returns
+    -------
+    loaded_obj: pickle object
+                Pickle which was contained in the path given as parameter.
+    """
+
+    with open(path,"rb") as f:
+        loaded_obj = pickle.load(f)
+    return loaded_obj
+
+
+def save_pickle(path: str, obj: Any) -> None:
+    """
+    Given a path and an object, stores the object as a pickle file in the specified path.
+
+    Parameters
+    ----------
+    path : string
+           String represents the path where the pickle object will be stored.
+
+    obj : any
+          Represents the object that will be stored.
+
+    Returns
+    -------
+    None : None
+    """
+
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
