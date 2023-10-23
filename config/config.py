7f3da63162a0e8b51c5ab07c8aef12fd4f2387f8
Joao Melo <joaohfpmelo@gmail.com>
2023-10-23 17:42:51 +0200

    deleted categorical cols from config

--- config/config.py
+++ config/config.py
@@ -3,5 +3,3 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DATA_PATH = PROJECT_ROOT / "data" / "abalone.csv"
 MODEL_PATH = PROJECT_ROOT / "src" / "web_service" / "local_objects" / "model.pkl"
-
-CATEGORICAL_COLS = ["Sex"]
